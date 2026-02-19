b4ce2802d081b7e1a8d0a6e09f4efa7c5c7ec84d
sendo1639 <gustavo.ifi.leg@gmail.com>
2026-02-19 13:41:47 -0300

    Correção

---
+++ Dashboard.bat
@@ -0,0 +1,6 @@
+﻿@ECHO OFF
+ECHO Hello World! Your first batch file was printed on the screen successfully.
+SET PYTHON_EXEC=".\ambiente\Scripts\streamlit.exe"
+SET DASHBOARD=".\frontend_dashboard\Home.py" 
+START  "PyScript" %PYTHON_EXEC% run %DASHBOARD%
+PAUSE 
